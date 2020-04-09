e6d98ccd1d6988ff74e331e79664d26742dce7cb
Adriano Luis de Almeida <adrianoo.luis.almeida@gmail.com>
2020-04-09 18:02:35 -0300

    Criaçao da aplicaçao requisitada

---
+++ README.md
@@ -0,0 +1,30 @@
+### :rocket: Desafio 03: Conceitos do ReactJS Bootcamp GoStack 11.0
+Neste desafio foi solicitada a criação de uma aplicação para aplicar os conceitos iniciais aprendidos sobre Reacts.JS.
+
+### Tecnologias utilizadas
+
+- [Jest](https://www.npmjs.com/package/jest)
+- [Supertest](https://www.npmjs.com/package/supertest)
+
+
+### Backend da aplicação
+
+- [Desafio 2](https://github.com/adrianoluisalmeida/desafio-02-gostack11) - Para rodar a aplicação, basta entrar na tela e executar os seguintes comandos:
+
+```js
+yarn
+yarn dev
+```
+
+### :muscle: Projeto
+
+Aplicação de introdução ao react.js do bootcamp gostack 11.0 para armazenar repositórios em um portofólio, que permite a criação, atualização e remoção de repositórios.
+
+
+### :memo: Licença 
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE.md) para mais detalhes.
+
+---
+
+<p align="center">Feito  por <strong>Adriano Almeida</p>
--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
+  /**
+   * Add repositories
+   */
   async function handleAddRepository() {
-    // TODO
+
+    const reponse = await api.post('/repositories', {
+      title: `novo repository ${Date.now()}`,
+	    url: "http://github.com",
+	    techs: ["Node.js", "React.js"]
+    });
+
+    setRepositories([...repositories, reponse.data]);
   }
 
+  /**
+   * Remove Repository
+   * @param int id 
+   */
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+    repositories.splice(repositoryIndex, 1);
+
+    setRepositories([...repositories]);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          repositories.map(repository => (
+            <li key={repository.id}>
+              {repository.title} 
+              
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          ))
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
